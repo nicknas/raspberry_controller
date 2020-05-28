dbeeceeed92a237b01a1af5882f9ab0f36bfbac9
Nick <nicolas.alcaine@gruposantander.com>
2020-05-28 18:18:05 +0200

    Updated with comments

--- controller.py
+++ controller.py
@@ -2,12 +2,10 @@ from ibath_master_client import IBathMasterClient
 from constants import MQTT_BROKER, MQTT_PORT
 import time
 
-
-
-# Main program loop
+# Inicializamos el cliente con las constantes de broker y puerto
 ibath_master_client = IBathMasterClient(mqtt_broker=MQTT_BROKER, mqtt_port=MQTT_PORT)
+# Lanzamos el cliente
 ibath_master_client.start()
-
 
 while(1):
     pass
--- ibath_master_client.py
+++ ibath_master_client.py
@@ -8,6 +8,9 @@ class IBathMasterClient:
         self.mqtt_port = mqtt_port
         self.client = mqtt.Client("ibath_master_client")
 
+    # Función que decide qué tipo de luz usar en el baño
+    # Entre las 20:30 hasta las 6 de la mañana se emitirá luz cálida
+    # Por el contrario, se emite luz fría
     def process_control_light(self, payload: str):
         print('Recogido input para controlar la luz')
         time = datetime.strptime(payload, "%H:%M").time()
@@ -18,16 +21,24 @@ class IBathMasterClient:
             print('Enviado light a slave')
             self.client.publish('iBath-slave/control_light', 'light')
 
+    # Función que decide si el ventilador tiene que estar encendido
+    # Solo se activará si la temperatura es mayor a 25ºC
     def process_control_temperature(self, payload: str):
         print('Recogido input para controlar la temperatura')
         temperature = float(payload)
-        if temperature > 20.0:
+        if temperature > 25.0:
             print('Enviado on a slave')
             self.client.publish('iBath-slave/control_temperature', 'on')
         else:
             print('Enviado off a slave')
             self.client.publish('iBath-slave/control_temperature', 'off')
 
+    # Función que decide qué alarmas emitir en el baño
+    # Para el CO2, a partir de 1000 ppm el usuario puede tener dolores de cabeza y sensación de asfixia
+    # Por lo que emitiría una alarma de CO2 a partir de esta cantidad.
+    # A partir de una cantidad superior a un 60% de humedad, el usuario puede sentir asfixia,
+    # Por lo que emitiría una alarma de humedad.
+    # Si no hay riesgos, el controlador responde que no hay alarmas
     def process_control_humidity_co2(self, payload: str):
         print('Recogido input para controlar la humedad y co2')
         parameters = payload.split(',')
@@ -37,7 +48,7 @@ class IBathMasterClient:
         if co2 > 1000.0:
             alert += 'co2 alert'
         
-        if humidity > 30.0:
+        if humidity > 60.0:
             if len(alert) == 0:
                 alert += 'humidity alert'
             else:
@@ -51,6 +62,7 @@ class IBathMasterClient:
             print('Enviado no alert a slave')
             self.client.publish('iBath-slave/control_humidity_co2', 'no alert')
 
+    # Función listener que escucha los mensajes del slave
     def receive_slave_inputs(self, client, userdata, message):
         if str(message.topic) == 'iBath-master/control_light':
             self.process_control_light(str(message.payload.decode("utf-8")))
@@ -61,12 +73,13 @@ class IBathMasterClient:
         if str(message.topic) == 'iBath-master/control_humidity_co2':
             self.process_control_humidity_co2(str(message.payload.decode("utf-8"))) 
 
+    # Inicializamos todas las subscripciones y listeners
     def start(self):
         self.client.connect(self.mqtt_broker, self.mqtt_port)
-        self.client.subscribe("iBath-master/control_light") # Subscribe to the topic AC_unit
+        self.client.subscribe("iBath-master/control_light")
         self.client.subscribe("iBath-master/control_temperature")
         self.client.subscribe("iBath-master/control_humidity_co2")
-        self.client.on_message = self.receive_slave_inputs # Attach the messageFunction to subscription
-        self.client.loop_start() # Start the MQTT client
+        self.client.on_message = self.receive_slave_inputs
+        self.client.loop_start()
         
 
